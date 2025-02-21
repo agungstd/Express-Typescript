b92a9ca83236c22d313131c61baceb299935c540
agungstd <apakatadunia11@gmail.com>
2025-02-21 11:33:03 +0700

    Refactor CRUD operations for Barang model

--- src/services/barang.service.ts
+++ src/services/barang.service.ts
@@ -1,34 +1,29 @@
-import type BarangType from '../types/barang.type'
-import prisma from '../utils/client'
+import type BarangType from '../types/barang.type';
+import prisma from '../utils/client';
 
 export const getBarang = async (): Promise<BarangType[] | null> => {
-  const data = await prisma.barang.findMany()
-  return data
-}
+  return await prisma.barang.findMany();
+};
 
 export const getBarangById = async (id: number): Promise<BarangType | null> => {
-  const data = await prisma.barang.findUnique({ where: { id } })
-  return data
-}
+  return await prisma.barang.findUnique({ where: { id } });
+};
 
 export const insertBarang = async (
   payload: BarangType
 ): Promise<BarangType> => {
-  const data = await prisma.barang.create({ data: payload })
-  return data
-}
+  return await prisma.barang.create({ data: payload });
+};
 
 export const updateBarang = async (
   payload: BarangType
 ): Promise<BarangType> => {
-  const data = await prisma.barang.update({
+  return await prisma.barang.update({
     where: { id: payload.id },
-    data: { ...payload }
-  })
-  return data
-}
+    data: { ...payload },
+  });
+};
 
 export const deleteBarang = async (id: number): Promise<BarangType> => {
-  const data = await prisma.barang.delete({ where: { id } })
-  return data
-}
+  return await prisma.barang.delete({ where: { id } });
+};
