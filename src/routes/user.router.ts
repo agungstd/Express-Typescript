a04f0aad588e905ee72a84a4dd07da4cbb67023d
agungstd <apakatadunia11@gmail.com>
2025-03-14 09:01:33 +0700

    Add logout route to user router

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -2,12 +2,14 @@ import { Router } from 'express'
 import {
   loginUser,
   refreshToken,
-  registerUser
+  registerUser,
+  logoutUser
 } from '../controllers/user.controller'
 const userRouter = Router()
 
 userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
 userRouter.get('/refresh', refreshToken)
+userRouter.post('/logout', logoutUser)
 
 export default userRouter
